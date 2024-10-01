07f6e59692a67ef132019af0e54152a63abc0d38
dhruvigopani <dhruvigopani@gmail.com>
2024-10-01 11:40:40 -0500

    day 1

---
+++ day1.py
@@ -0,0 +1,7 @@
+#learnt about printing, input, and variables
+
+print("\n🎸🥁 Welcome to the Band Name Generator 🎸🥁\n")
+city = input("What's the name of the city you are from? ")
+pet = input("What is the name of your pet? ")
+len_pet = len(pet)
+print(f"\nYour Band Name could be {city} {pet} {len_pet}!\n")
